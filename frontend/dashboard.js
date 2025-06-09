7b9b00291a70c22c13cb5d9c69ee39b204bd6678
hfizmzr <153249845+hfizmzr@users.noreply.github.com>
2025-06-10 02:15:14 +0800

    Update dashboard.js

--- frontend/dashboard.js
+++ frontend/dashboard.js
@@ -24,7 +24,7 @@ const countryColors = ['#3b82f6', '#10b981', '#f59e0b', '#ef4444', '#6366f1', '#
 // const API_BASE_URL = `https://ishowspeedanalysisdashboard.onrender.com/api`;
 const API_BASE_URL = window.location.hostname === "localhost"
   ? "http://localhost:5000/api"
-  : "https://ishowspeed-backend.onrender.com/api";
+  : "https://ishowspeedanalysisdashboard.onrender.com/api";
 
 
 document.addEventListener('DOMContentLoaded', async function() {
