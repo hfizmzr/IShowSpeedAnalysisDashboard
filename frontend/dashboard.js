177eb4b90e7545cf7cca352fe48fc837af8b8462
hfizmzr <153249845+hfizmzr@users.noreply.github.com>
2025-06-10 01:39:04 +0800

    Update dashboard.js

--- frontend/dashboard.js
+++ frontend/dashboard.js
@@ -20,7 +20,8 @@ const countryColors = ['#3b82f6', '#10b981', '#f59e0b', '#ef4444', '#6366f1', '#
 
 
 // Define API_BASE_URL in the global scope
-const API_BASE_URL = `${window.location.protocol}//${window.location.hostname}/api`;
+// const API_BASE_URL = `${window.location.protocol}//${window.location.hostname}/api`;
+const API_BASE_URL = `https://ishowspeedanalysisdashboard.onrender.com/api`;
 
 
 document.addEventListener('DOMContentLoaded', async function() {
